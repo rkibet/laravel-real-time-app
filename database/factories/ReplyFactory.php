ad83424f87c72b392630d8bd4e418a07e2a8f342
rkibet <ronkibet2012@gmail.com>
2020-06-08 01:58:33 +0300

    Factory and Seed

--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -5,8 +5,15 @@
 use App\Model\Reply;
 use Faker\Generator as Faker;
 
+
 $factory->define(Reply::class, function (Faker $faker) {
     return [
-        //
+        'body' => $faker->text,
+        'question_id' => function (){
+            return \App\Model\Question::all()->random();
+        },
+        'user_id' => function(){
+            return \App\User::all()->random();
+        },
     ];
 });
