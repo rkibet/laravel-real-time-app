54c8a13c11dd11d65e1f684f06be20c55dca4c0a
rkibet <ronkibet2012@gmail.com>
2020-06-10 23:47:36 +0300

    CRUD reply

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 Route::apiResource('/question','QuestionController');
 Route::apiResource('/category','CategoryController');
+Route::apiResource('/question/{question}/reply','ReplyController');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
